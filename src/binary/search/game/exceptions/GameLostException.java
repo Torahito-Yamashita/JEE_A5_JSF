720f96582b8d9cef37747b5f932282221a03a076
Torahito-Yamashita <ho.moko.niki.aki2017@gmail.com>
2017-12-01 08:40:28 -0500

    All work files added

---
+++ src/binary/search/game/exceptions/GameLostException.java
@@ -0,0 +1,29 @@
+package binary.search.game.exceptions;
+
+public class GameLostException extends Exception {
+
+	public GameLostException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GameLostException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GameLostException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GameLostException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GameLostException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
